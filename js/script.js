4df80bd3b11e2b428a8ef4ead8ace9fddbf24252
G-Ulrich5000 <95188403+G-Ulrich5000@users.noreply.github.com>
2021-12-02 18:03:10 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+/* Button Up*/
 var mybutton = document.getElementById("myBtn");
 
 
@@ -15,4 +15,19 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+/* Acordion */
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
 }
